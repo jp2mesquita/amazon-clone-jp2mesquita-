6a40f99755bd61b267e2d242b57486208aa5dd2a
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-07 17:41:05 -0300

    feat: Add SSR to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+import { api } from "../lib/axios";
 
-export default function Home() {
+export interface ProductProps{
+  id: number,
+  title: string,
+  price: number,
+  description: string,
+  category: string,
+  image: string,
+  rating?:{
+    rate: number,
+    count: number
+  }
+}
+
+export default function Home(products: ProductProps[]) {
+  
+
   return (
     <div className=''>
       <Head>
@@ -13,7 +31,25 @@ export default function Home() {
 
       <main className="max-w-screen-2xl mx-auto">
         <Banner />
+
+        <ProductFeed product={products}/>
       </main>
     </div>
   )
+}
+
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const products = await api.get('/products')
+
+
+
+ 
+  return{
+    props:{
+      products: products.data
+    }
+  }
 }
