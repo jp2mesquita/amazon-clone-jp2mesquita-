5ab28ba08d0c9de7e25ae1eb23664313e8298a0a
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-07 10:01:33 -0300

    fix: change some size and padding values

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ import {
 export default function Header(){
   return(
     <header>
-      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+      <div className="flex items-center bg-amazon_blue  flex-grow py-2">
         <div className="mt-2 px-4 flex items-center flex-grow sm:flex-grow-0">
           <Image 
             src='https://links.papareact.com/f90'
             alt=''
-            width={150}
-            height={40}
+            width={100}
+            height={30}
             quality={100}
             className='object-contain cursor-pointer'
           />
@@ -25,7 +25,7 @@ export default function Header(){
             type="text" 
             className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"
           />
-          <SearchIcon className="h-12 p-4"/>
+          <SearchIcon className="h-14 p-4"/>
         </div>
 
         <div className="text-white flex items-center text-xs gap-6 mx-6 whitespace-nowrap ">
