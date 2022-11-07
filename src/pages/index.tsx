702862e6571813a602c1a2ddcb075eacf5d007fb
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-07 10:02:53 -0300

    feat: create banner component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className=''>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+      </main>
     </div>
   )
 }
