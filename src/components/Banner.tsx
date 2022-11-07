0a4a32ba691d7cf3c567d8a3f14f1b6d0539dc17
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-07 10:44:19 -0300

    feat: add banner content

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,58 @@
+import Image from 'next/image'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+
 export default function Banner(){
+  
+  const images = [
+    "https://links.papareact.com/gi1",
+    "https://links.papareact.com/6ff",
+    "https://links.papareact.com/7ma"
+  ]
+
   return(
-    <div>
-       
+    <div className="relative">
+
+      <div className='absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20'/>
+
+      <Carousel
+      autoPlay
+      infiniteLoop
+      showStatus={false}
+      showIndicators={false}
+      showThumbs={false}
+      interval={5000}
+      renderThumbs={
+        () => images.map((item) => (
+          <div key='item'>
+            <Image 
+              src={item}
+              alt=''
+              fill
+              className='objetc-contain'
+            />
+          </div>
+        ))
+      }
+      >
+
+        {images.map( (item) =>{
+          return(
+            <Image 
+              key={item}
+              src={item}
+              alt=''
+              width={1080}
+              height={760}
+              loading='lazy'
+              quality={100}
+              
+              
+            />
+          )
+        })}
+      
+      </Carousel>
     </div>
   )
 }
