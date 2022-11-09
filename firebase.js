b93d9fbb4b23fbb2719d7dcd0750372eba49b6fa
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-09 13:59:39 -0300

    chore: create and add firebase config file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWUd2JFv3Mbxstb8zPgFN664AR9zanPeo",
+  authDomain: "amzn-2-clone-1a049.firebaseapp.com",
+  projectId: "amzn-2-clone-1a049",
+  storageBucket: "amzn-2-clone-1a049.appspot.com",
+  messagingSenderId: "725232187623",
+  appId: "1:725232187623:web:b09e8a834966190c9c4a42"
+};
