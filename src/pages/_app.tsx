85febcf6ecb3bd32aed7bd328add6e11f6a4d29a
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-09 17:36:38 -0300

    chore: implement the provider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { store } from '../app/store'
 import { SessionProvider  } from 'next-auth/react'
 
 export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
 
     </SessionProvider>
   )
