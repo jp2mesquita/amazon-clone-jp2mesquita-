74565115b759743086101cb9279a7dfa11a25dee
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-10 13:22:15 -0300

    feat: create the conection with stripe api

---
+++ src/lib/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from 'stripe'
+
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY, 
+  {
+    apiVersion: "2022-08-01",
+    appInfo: {
+      name: 'Amazom-2.0',
+    }
+  }
+)
