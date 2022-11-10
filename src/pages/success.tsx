9bf02682ad7a60242b17153387e6efc80291bcdb
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-10 13:23:41 -0300

    feat: create the  empty success page

---
+++ src/pages/success.tsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Success(){
+  return(
+    <div>
+      <Header />
+      
+      <h1>Success in your buying!</h1>
+    </div>
+  )
+}
