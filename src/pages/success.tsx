10358dac9341cb0f518b273df83640324e1a18b1
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-11 09:08:11 -0300

    feat: create success page content

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,11 +1,52 @@
+import { CheckCircleIcon } from "@heroicons/react/solid";
+import { GetServerSideProps } from "next";
+import { unstable_getServerSession } from "next-auth";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 export default function Success(){
+
+  const router = useRouter()
+
   return(
-    <div>
+    <div className="bg-gray-100 h-screen">
       <Header />
       
-      <h1>Success in your buying!</h1>
+      <main className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col p-10 bg-white">
+          <div className="flex items-center space-x-2 mb-5">
+            <CheckCircleIcon className="text-green-600 h-10"/>
+            <h1 className="text-3xl">
+              Thank You, your order has been confirmed
+            </h1>
+          </div>
+          <p>
+            Thank you for shopping with us. We&apos;ll send a confirmation ons your item has shipped. If you would like to check the status of your order(s) please press the link below. 
+          </p>
+          <button 
+            onClick={() => router.push('/orders')}
+            className="button mt-8"
+          >
+            Go to My orders
+          </button>
+        </div>
+      </main>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  const session = await unstable_getServerSession(
+    context.req,
+    context.res,
+    authOptions
+  )
+
+  return{
+    props:{
+      session
+    }
+  }
 }
