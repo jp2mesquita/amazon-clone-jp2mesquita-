46891b428e59203bd082a3eed5e69ea84125ca8f
João Paulo Mesquita <jp-mesquita@live.com>
2022-11-14 13:04:22 -0300

    feat: add a progress bar in page transitions with @badrap/bar-of-progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,22 @@ import { Provider } from 'react-redux'
 import { store } from '../app/store'
 import { SessionProvider  } from 'next-auth/react'
 
+
+import Router from 'next/router'
+import ProgressBar from '@badrap/bar-of-progress'
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FF9900',
+  className: 'z-50',
+  delay: 10,
+})
+
+Router.events.on('routeChangeStart', progress.start)
+Router.events.on('routeChangeComplete', progress.finish)
+Router.events.on('routeChangeError', progress.finish)
+
+
 export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
   return (
     <SessionProvider session={session}>
